748468216717ff2b79d89ecd0430c3b99f4c5e14
Quentin Colle <quentin41500@gmail.com>
2018-11-29 17:03:47 -0500

    Feature - Hierarchy Modules
    - Changed hierarchy of the modules to have custom modules at the top
    - Improve Mock Mode

--- birdo/src/main/java/com/prolificinteractive/birdo/utils/MockMode.kt
+++ birdo/src/main/java/com/prolificinteractive/birdo/utils/MockMode.kt
@@ -3,13 +3,22 @@ package com.prolificinteractive.birdo.utils
 import android.content.Context
 import android.content.SharedPreferences
 
+/**
+ * Util class for working with mock.
+ */
 class MockMode(context: Context) {
   private val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILE_NAME, 0)
 
+  /**
+   * Whether the mock mode is activated.
+   */
   fun isMockMode(): Boolean {
     return prefs.getBoolean(IS_MOCK_MODE, false)
   }
 
+  /**
+   * The delay set for mock.
+   */
   fun getNetworkDelay(): Long {
     return prefs.getLong(NETWORK_DELAY, 0L)
   }
