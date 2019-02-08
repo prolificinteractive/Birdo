03fb1a781ecce13358add2a001faccd39c64d096
Quentin Colle <quentin41500@gmail.com>
2019-02-08 11:29:21 -0500

    Chore - Update Dependencies
    - Updated Kotlin Version
    - Updated Gradle Version
    - Added Documentation and Sources to Artifact

--- app/src/main/java/com/prolificinteractive/sample/api/RickAndMortyApi.kt
+++ app/src/main/java/com/prolificinteractive/sample/api/RickAndMortyApi.kt
@@ -5,5 +5,5 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface RickAndMortyApi {
-  @GET("  character/") fun getCharacters(): Call<Characters>
+  @GET("character/") fun getCharacters(): Call<Characters>
 }
